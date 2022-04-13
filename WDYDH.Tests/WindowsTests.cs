0b81660640da42a96736878d14ec2247530ef067
Jarred Capellman <jcapellman@hotmail.com>
2022-04-12 20:51:40 -0500

    Added: All inclusive unit test and json output

--- WDYDH.Tests/WindowsTests.cs
+++ WDYDH.Tests/WindowsTests.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Versioning;
+using System.Text.Json;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using WDYDH.lib.Implementations;
+using WDYDH.lib.Objects;
 
 namespace WDYDH.Tests
 {
@@ -10,6 +15,48 @@ namespace WDYDH.Tests
     [SupportedOSPlatform("windows")]
     public class WindowsTests
     {
+        public class HardwareInfo
+        {
+            public List<CPUInformation> CPUs { get; set; }
+
+            public BIOSInformation Bios { get; set; }
+
+            public List<NetworkAdapter> NetworkAdapters { get; set; }
+
+            public List<GPU> GPUs { get; set; }
+
+            public List<Mouse> Mice { get; set; }
+
+            public List<StorageDevice> StorageDevices { get; set; }
+
+            public List<SystemMemory> SystemMemories { get; set; }
+
+            public List<Device> Devices { get; set; }
+        }
+
+
+        [TestMethod]
+        public void AllTheThings()
+        {
+            var winInfo = new WindowsHardwareInformation();
+
+            var hardwareInfo = new HardwareInfo
+            {
+                CPUs = winInfo.CPUInformation,
+                Bios = winInfo.BIOSInformation,
+                Devices = winInfo.SystemDeviceDrivers,
+                GPUs = winInfo.GPUs,
+                Mice = winInfo.Mice,
+                NetworkAdapters = winInfo.NetworkAdapters ?? new List<NetworkAdapter>(),
+                StorageDevices = winInfo.StorageDevices ?? new List<StorageDevice>(),
+                SystemMemories = winInfo.SystemMemory,
+            };
+
+            var json = JsonSerializer.Serialize(hardwareInfo);
+
+            System.IO.File.WriteAllText(System.IO.Path.Combine(AppContext.BaseDirectory, "hardware.json"), json);
+        }
+
         [TestMethod]
         public void CPUInformation()
         {
